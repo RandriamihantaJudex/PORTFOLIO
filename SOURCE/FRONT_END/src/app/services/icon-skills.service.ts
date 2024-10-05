6de291a8edf0a2aea6015db525b893bbe24c433c
randriamihantajudex <randriamihantajudex@gmail.com>
2024-10-05 18:27:45 +0200

    about me

--- SOURCE/FRONT_END/src/app/services/icon-skills.service.ts
+++ SOURCE/FRONT_END/src/app/services/icon-skills.service.ts
@@ -1,20 +1,9 @@
-import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
-import { ProjectComponent } from '../../composant/project/project.component';
-import { SkillsComponent } from '../../composant/skills/skills.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-@Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [ProjectComponent, SkillsComponent,FontAwesomeModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
 })
-export class HomeComponent implements AfterViewInit {
-  @ViewChild('nom') nom!: ElementRef
-  @ViewChild('lineOne') lineOne!: ElementRef
-  @ViewChild('lineTwo') lineTwo!: ElementRef
- 
+export class IconSkillsService {
 
   langages: Array<string> = [
     'https://raw.githubusercontent.com/bablubambal/All_logo_and_pictures/7c0ac2ceb9f9d24992ec393d11fa7337d2f92466/others/html.svg',
@@ -35,15 +24,5 @@ tools:Array<string>=[
   '../../../assets/images/git.svg',
   '../../../assets/images/github.svg',
 ]
-
-// ICONS
-arrow=faArrowRight
-
-  ngAfterViewInit(): void {
-    this.lineOne.nativeElement.style.setProperty('--largeur', `${this.nom.nativeElement.clientWidth}px`);
-    this.lineTwo.nativeElement.style.setProperty('--largeurTwo', `${this.nom.nativeElement.clientWidth - (this.nom.nativeElement.clientWidth / 3)}px`);
-  }
-
-
 
 }
